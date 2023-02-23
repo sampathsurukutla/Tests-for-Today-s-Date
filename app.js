49d894c5042aa6f6384d910f25c5db87f4c713d2
sampath <sampathsurukutla12@gmail.com>
2023-02-23 06:22:38 +0000

    Tests for Today's Date

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (request, response) => {
+  const date = new Date();
+  response.send(
+    `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`
+  );
+});
+
+module.exports = app;
